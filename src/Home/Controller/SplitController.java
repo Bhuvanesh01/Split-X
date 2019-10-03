55a60fd2f3a8d8661ba5c5abf66acece89769a08
Bhuvanesh <bhuvaneshmishra56@gmail.com>
2019-10-03 12:56:11 +0530

    Added Some changes to Split Scene Window

---
+++ src/Home/Controller/SplitController.java
@@ -0,0 +1,168 @@
+package Home.Controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SplitController implements Initializable {
+    private double x, y;
+    String Path = null;
+    @FXML
+    private Button Browse;
+
+    @FXML
+    private Button Home;
+
+    @FXML
+    private Button JoinMenu;
+
+    @FXML
+    private Button SplitMenu;
+
+    @FXML
+    private Button Help;
+
+    @FXML
+    private Button About;
+
+    @FXML
+    private Button Close;
+
+    @FXML
+    private JFXButton BtnClose;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+    @FXML
+    private void  HandleClicks(ActionEvent event) throws IOException
+    {
+        if (event.getSource() == Home)
+        {
+            Parent HomeParent = FXMLLoader.load(getClass().getResource("../FXML/Home.fxml"));
+            Scene HomeScene = new Scene(HomeParent);
+            Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            window.setScene(HomeScene);
+            window.show();
+            HomeParent.setOnMousePressed(event1 -> {
+                x = event1.getSceneX();
+                y = event1.getSceneY();
+            });
+            HomeParent.setOnMouseDragged(event1 -> {
+
+                window.setX(event1.getScreenX() - x);
+                window.setY(event1.getScreenY() - y);
+
+            });
+        }
+        else
+        {
+
+        }
+        if (event.getSource() == JoinMenu)
+        {
+            Parent JoinParent = FXMLLoader.load(getClass().getResource("../FXML/Join.fxml"));
+            Scene SplitScene = new Scene(JoinParent);
+            Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            window.setScene(SplitScene);
+            window.show();
+            JoinParent.setOnMousePressed(event1 -> {
+                x = event1.getSceneX();
+                y = event1.getSceneY();
+            });
+            JoinParent.setOnMouseDragged(event1 -> {
+
+                window.setX(event1.getScreenX() - x);
+                window.setY(event1.getScreenY() - y);
+
+            });
+        }
+        else
+        {
+
+        }
+        if (event.getSource() == SplitMenu)
+        {
+            Parent SplitParent = FXMLLoader.load(getClass().getResource("../FXML/Split.fxml"));
+            Scene SplitScene = new Scene(SplitParent);
+            Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            window.setScene(SplitScene);
+            window.show();
+            SplitParent.setOnMousePressed(event1 -> {
+                x = event1.getSceneX();
+                y = event1.getSceneY();
+            });
+            SplitParent.setOnMouseDragged(event1 -> {
+
+                window.setX(event1.getScreenX() - x);
+                window.setY(event1.getScreenY() - y);
+
+            });
+        }
+        else
+        {
+
+        }
+        if (event.getSource() == Help)
+        {
+
+        }
+        else
+        {
+
+        }
+        if (event.getSource() == About)
+        {
+
+        }
+        else
+        {
+
+        }
+        if (event.getSource() == Browse)
+        {
+            FileChooser fc = new FileChooser();
+            File SelectedFile = fc.showOpenDialog(null);
+            if (SelectedFile != null)
+            {
+                Path = SelectedFile.getAbsolutePath();
+            }
+            else
+            {
+                System.out.println("Not A Valid File");
+            }
+        }
+
+    }
+
+    @FXML
+    private void ExitApp(MouseEvent event)
+    {
+        if (event.getSource() == Close)
+        {
+            System.exit(0);
+        }
+        else if (event.getSource() == BtnClose)
+        {
+            System.exit(0);
+        }
+    }
+}
+
+
+
